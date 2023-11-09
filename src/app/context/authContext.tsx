51d7e284f0a01b7b63061ed2228792bec2b6cbd5
aarevaloe <aarevalo@duppla.co>
2023-11-09 18:31:19 -0500

    Importación form login y creación contexto-firebase

---
+++ src/app/context/authContext.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState, ReactNode } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
+import {auth} from '../ConfigFirebase/firebase'
+import { UserCredential } from "firebase/auth";
+import { User } from "firebase/auth";
+
+
+
+
+interface AuthContextType {
+    singUp: (email: string, password: string) => Promise<UserCredential>;
+    login: (email: string, password: string) => Promise<UserCredential>;
+    logout: () => Promise<void>;
+    user: User | null;
+    loading: boolean;
+  }
+const authContext = createContext<AuthContextType | undefined>(undefined);
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(authContext);
+  if (!context) throw new Error('useAuth debe estar dentro del proveedor');
+  return context;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export default function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const singUp = async (email: string, password: string): Promise<UserCredential> => {
+    const credential = await createUserWithEmailAndPassword(auth, email, password);
+    setUser(credential.user);
+    return credential;
+  };
+  const login = async (email: string, password: string): Promise<UserCredential> => {
+    const credential = await signInWithEmailAndPassword(auth, email, password);
+    setUser(credential.user);
+    return credential;
+  };
+  const logout = async (): Promise<void> => signOut(auth);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    })
+    return () => unsubscribe();
+  }, [])
+
+  const contextValue: AuthContextType = {
+    singUp,
+    login,
+    logout,
+    user,
+    loading
+  };
+
+  return (
+    <authContext.Provider value={contextValue}>
+      {children}
+    </authContext.Provider>
+  )
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,107 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import AuthProvider from './authContext/authContext'
+import LogoInversionistas from '../img/logoinversionistas.svg'
+import { Box, Button, Container, CssBaseline, TextField, Typography } from '@mui/material';
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+
+    <AuthProvider>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <div>
+            <Image src={LogoInversionistas} alt="" width={492} height={287} />
+          </div>
+          <Typography component="h1" variant='h5' sx={{
+            marginTop: '18px',
+            color: '#ffffff',
+            fontFamily: 'Roboto',
+            fontStyle: 'normal',
+            fontSize: '36px',
+            fontWeight: 'bold',
+
+          }}>
+            Portal Inversionistas
+          </Typography>
+          <Box component="form" noValidate sx={{ marginTop: '35px' }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              sx={{
+              }}
             />
-          </a>
-        </div>
-      </div>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              sx={{
+                labelColor: '#ffffff',
+              }}
+            />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                marginTop: '40px',
+                mb: 2,
+                background: '#6C9FFF',
+                borderRadius: '10px',
+                color: '#FFFFFF',
+              }}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            >
+              Iniciar sesión
+            </Button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <Link href='/Register'>
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 0, mb: 2,
+                  background: '#6C9FFF',
+                  borderRadius: '10px',
+                  color: '#FFFFFF',
+                  marginBottom: '80px',
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+                }}
+              >
+                Registrarse
+              </Button>
+            </Link>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </Box>
+        </Box>
+
+      </Container>
+    </AuthProvider>
   )
 }
