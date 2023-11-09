8776381c710681ab8a959d978d3fe3537f35e8c3
aarevaloe <aarevalo@duppla.co>
2023-11-09 18:44:00 -0500

    prueba despliegue authcontext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import AuthProvider from './authContext/authContext'
+import AuthProvider from '../app/context/authContext';
 import LogoInversionistas from '../img/logoinversionistas.svg'
 import { Box, Button, Container, CssBaseline, TextField, Typography } from '@mui/material';
 import Link from 'next/link';
