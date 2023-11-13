c8863d959645118b6079d749f922046cca80218d
aarevaloe <aarevalo@duppla.co>
2023-11-13 17:59:39 -0500

    Navbar-pueba-d

--- src/app/dashboard/buyer/page.tsx
+++ src/app/dashboard/buyer/page.tsx
@@ -6,10 +6,16 @@ import { Box } from "@mui/material";
 
 function Page() {
  
+    const isBrowser = typeof window !== 'undefined';
 
     return (
 
         <Box sx={{ display: 'flex', backgroundColor: 'red' }}>
+            {isBrowser && window.location && window.location.pathname === '/dashboard/buyer' && (
+                <div>
+                    <h1> Buyer-inversionista pequeño</h1>
+                </div>
+            )}
         <div>
             
             <h1> Buyer-inversionista pequeño</h1>
