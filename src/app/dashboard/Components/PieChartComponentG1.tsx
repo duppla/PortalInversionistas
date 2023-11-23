08818ab78c8e74ab71d8f889bf6ebd4711879141
aarevaloe <aarevalo@duppla.co>
2023-11-22 23:19:03 -0500

    Componente G1

--- src/app/dashboard/Components/PieChartComponentG1.tsx
+++ src/app/dashboard/Components/PieChartComponentG1.tsx
@@ -70,19 +70,59 @@ function PieChartComponentG1() {
     }, []);
 
 
-/* Itera la data para mostrarla y elemina el campo total */
-    const formattedDataPie: MayHaveLabel[] = responseData
-        ? Object.keys(responseData[selectedDataKey])
-            .filter((key) => key !== 'total')
-            .map((key: string) => {
+            const getColorByKey = (key: string): string => {
+                switch (key) {
+                  case 'a_tiempo':
+                    return 'green';
+                  case 'en_mora':
+                    return 'red';
+                  default:
+                    return 'gray';
+                }
+              };
+
+        const formattedDataPie = responseData
+        ? responseData[selectedDataKey]
+          ? Object.keys(responseData[selectedDataKey])
+              .filter((key) => key !== 'total') // Filtra la categoría 'total'
+              .map((key: string) => {
                 const item = responseData[selectedDataKey][key];
+                let categoryLabel = key;
+      
+                // Personaliza los nombres de las categorías
+                if (key === 'a_tiempo') {
+                  categoryLabel = 'A tiempo';
+                } else if (key === 'en_mora') {
+                  categoryLabel = 'En mora';
+                }
+      
                 return {
-                    id: key,
-                    label: key,
-                    value: item,
+                  id: key,
+                  label: categoryLabel,
+                  value: item * 100,
+                  formattedValue: `${(item * 100).toFixed(2)}%`,
+                  color: getColorByKey(key), // Reemplaza getColorByKey con tu lógica de asignación de colores
                 };
-            })
+              })
+          : []
         : [];
+      
+
+        const formatTooltip = ({ id, value }: { id: string; value: number }) => {
+            let categoryLabel = id;
+          
+            // Personaliza los nombres de las categorías
+            if (id === 'a_tiempo') {
+              categoryLabel = 'Tiempo';
+            } else if (id === 'en_mora') {
+              categoryLabel = 'Mora';
+            }
+          
+            return `${categoryLabel}: ${value}`;
+          };
+          
+    
+       
 
     /* Función para actualizar la selección del usuario */
     const handleDataSelection = (dataKey: string) => {
@@ -97,14 +137,8 @@ function PieChartComponentG1() {
     };
 
 
-   /*  console.log('formattedDataPie', formattedDataPie); */
-
-
-
-
-
     return (
-        <div className='grafica-piecharts ' >
+        <div className="grafica-piecharts" style={{ position: 'relative', width: '100%', height: '380px' }}>
             <div>
                 <FormControl fullWidth>
                     <Grid container spacing={2} alignItems="center" sx={{ borderBottom: '1px solid #9B9EAB' }}>
@@ -118,13 +152,14 @@ function PieChartComponentG1() {
                                 value={selectedValue}
                                 label="Age"
                                 onChange={handleSelectChange}
-                                /*  IconComponent={() => <KeyboardArrowDownIcon />} */
-
                                 sx={{
-                                    color: '#9B9EAB', justifyContent: 'flex-end', textAlign: 'end', fill: '#ffffff', '&.MuiSelect-icon': { color: '#FFFFFF !important' },
+                                    color: '#9B9EAB',
+                                    justifyContent: 'flex-end',
+                                    textAlign: 'end',
+                                    fill: '#ffffff',
+                                    '&.MuiSelect-icon': { color: '#FFFFFF !important' },
                                     '& .MuiOutlinedInput-notchedOutline': { border: 'none' },
                                     '&:hover .MuiOutlinedInput-notchedOutline': { border: 'none' },
-
                                 }}
                             >
                                 <MenuItem value='este_anho'>Este año</MenuItem>
@@ -137,91 +172,117 @@ function PieChartComponentG1() {
             </div>
 
             {formattedDataPie.length > 0 && (
+                <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+                    <ResponsivePie
+                        data={formattedDataPie}
+                        margin={{ top: 40, right: 80, bottom: 80, left: -40 }}
+                        startAngle={0}
+                        innerRadius={0.7}
+                        padAngle={1}
+                        activeInnerRadiusOffset={3}
+                        activeOuterRadiusOffset={8}
+                        colors={['#6C9FFF', '#BAFCC5']}
+                        borderColor={{
+                            from: 'color',
+                            modifiers: [['darker', 0.2]],
+                        }}
+                        enableArcLinkLabels={false}
+                        arcLinkLabelsSkipAngle={10}
+                        arcLinkLabelsTextColor="#333333"
+                        arcLinkLabelsThickness={2}
+                        arcLinkLabelsColor={{ from: 'color' }}
+                        enableArcLabels={false}
+                        arcLabelsRadiusOffset={0.1}
+                        arcLabelsSkipAngle={10}
+                        arcLabelsTextColor={{
+                            from: 'color',
+                            modifiers: [['darker', 2]],
+                        }}
+                        defs={[
+                            {
+                                id: 'dots',
+                                type: 'patternDots',
+                                background: 'inherit',
+                                color: 'rgba(255, 255, 255, 0.3)',
+                                size: 4,
+                                padding: 1,
+                                stagger: true,
+                            },
+                            {
+                                id: 'lines',
+                                type: 'patternLines',
+                                background: 'inherit',
+                                color: 'rgba(255, 255, 255, 0.3)',
+                                rotation: -45,
+                                lineWidth: 6,
+                                spacing: 10,
+                            },
+                        ]}
+                       tooltip={(tooltipProps) => {
+                            const { id, value, color, formattedValue , label} = tooltipProps.datum;
+                          
+                            return (
+                              <div
+                                style={{
+                                  background: '#000',
+                                  color: color, // Usa el color personalizado asignado
+                                  padding: '10px',
+                                  borderRadius: '5px',
+                                  fontSize: '14px',
+                                }}
+                              >
+                                <div>
+                                  <strong> {label}: {formattedValue}%</strong>
+                                </div>                                
+                              </div>
+                            );
+                          }} 
+                          
+                        legends={[
+                            {
+                                anchor: 'right',
+                                direction: 'column',
+                                justify: false,
+                                translateX: 68,
+                                translateY: 1,
+                                itemsSpacing: 7,
+                                itemWidth: 111,
+                                itemHeight: 35,
+                                itemTextColor: '#999',
+                                itemDirection: 'left-to-right',
+                                itemOpacity: 1,
+                                symbolSize: 17,
+                                symbolShape: 'circle',
 
-                <ResponsivePie
-                    data={formattedDataPie}
-                    margin={{ top: 40, right: 80, bottom: 80, left: -20 }}
-                    startAngle={0}
-                    innerRadius={0.7}
-                    padAngle={1}
-                    activeInnerRadiusOffset={3}
-                    activeOuterRadiusOffset={8}
-                    colors={['#6C9FFF', '#BAFCC5',]}
-                    borderColor={{
-                        from: 'color',
-                        modifiers: [
-                            ['darker', 0.2]
-                        ]
-                    }}
-                    enableArcLinkLabels={false}
-                    arcLinkLabelsSkipAngle={10}
-                    arcLinkLabelsTextColor="#333333"
-                    arcLinkLabelsThickness={2}
-                    arcLinkLabelsColor={{ from: 'color' }}
-                    enableArcLabels={false}
-                    arcLabelsRadiusOffset={0.1}
-                    arcLabelsSkipAngle={10}
-                    arcLabelsTextColor={{
-                        from: 'color',
-                        modifiers: [
-                            ['darker', 2]
-                        ]
-                    }}
-                    defs={[
-                        {
-                            id: 'dots',
-                            type: 'patternDots',
-                            background: 'inherit',
-                            color: 'rgba(255, 255, 255, 0.3)',
-                            size: 4,
-                            padding: 1,
-                            stagger: true
-                        },
-                        {
-                            id: 'lines',
-                            type: 'patternLines',
-                            background: 'inherit',
-                            color: 'rgba(255, 255, 255, 0.3)',
-                            rotation: -45,
-                            lineWidth: 6,
-                            spacing: 10
-                        }
-                    ]}
-
-                    legends={[
-                        {
-                            anchor: 'right',
-                            direction: 'column',
-                            justify: false,
-                            translateX: 68,
-                            translateY: 1,
-                            itemsSpacing: 7,
-                            itemWidth: 111,
-                            itemHeight: 35,
-                            itemTextColor: '#999',
-                            itemDirection: 'left-to-right',
-                            itemOpacity: 1,
-                            symbolSize: 17,
-                            symbolShape: 'circle',
-                            effects: [
-                                {
-                                    on: 'hover',
-                                    style: {
-                                        itemTextColor: '#000'
+                                effects: [
+                                    {
+                                        on: 'hover',
+                                        style: {
+                                            itemTextColor: '#000'
+                                        }
                                     }
-                                }
-                            ]
-                        }
-                    ]}
-                />
+                                ]
+                            }
+                        ]}
+                    />
 
 
-            )}
+                </div>
 
-
-
-        </div>
-    )
+            )
+            }
+            <div className="centrado" style={{ position: 'absolute', top: '60%', left: '40%', transform: 'translate(-50%, -50%)', zIndex: 1 }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography sx={{ color: "#ffffff", marginBottom: '8px', textAlign: 'center', fontWeight: '600', fontStyle: 'normal', fontSize: '36px' }}>
+                    {responseData[selectedDataKey].total.toLocaleString()}
+                    </Typography>
+                    <Typography sx={{ color: '#6E7880', textAlign: 'center', fontWeight: '400', fontStyle: 'normal', fontSize: '24px' }}>
+                        Total
+                    </Typography>
+                </div>
+            </div>
+        </div >
+    );
 }
 
 export default PieChartComponentG1
