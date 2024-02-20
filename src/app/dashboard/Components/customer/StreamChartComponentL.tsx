794aa34ea5b92aac21ec881487fb753c1e44b57c
aarevaloe <121042571+aarevaloe@users.noreply.github.com>
2024-02-20 16:03:52 -0500

    Ajuste api L

--- src/app/dashboard/Components/customer/StreamChartComponentL.tsx
+++ src/app/dashboard/Components/customer/StreamChartComponentL.tsx
@@ -49,24 +49,7 @@ function StreamChartComponentL() {
     const storedEmail = typeof window !== 'undefined' ? localStorage.getItem('userEmail') : null;
 
     const { userEmail } = useAuth();
-    const getQueryParameter = (userEmail: string | null): string => {
-        const emailToUse = storedEmail || userEmail || '';
 
-        if (!emailToUse) {
-            // En caso de que el correo electrónico no esté disponible
-            return "";
-        }
-        // Verifica el correo electrónico y devuelve el parámetro de consulta correspondiente
-        if (userEmail === "fcortes@duppla.co" || userEmail === "fernando@skandia.co") {
-            return "skandia";
-        } else if (userEmail === "aarevalo@duppla.co" || userEmail === "fernando@weseed.co") {
-            return "weseed";
-        } else if (userEmail === "scastaneda@duppla.co") {
-            return "disponible";
-        } 
-        // En caso de que el correo electrónico no coincida con ninguno de los casos anteriores
-        return "";
-    };
     const [data, setData] = useState<DataType | null>(null);
     const [responseData, setResponseData] = useState<any>(null);
     const [dataApi, setDataApi] = useState<DataType[]>([]);
@@ -76,11 +59,14 @@ function StreamChartComponentL() {
 
 
     useEffect(() => {
-        const queryParameter = getQueryParameter(userEmail);
+        if (!userEmail) {
+            return;
+        }
+        const queryParameter = userEmail;
         const fetchData = async () => {
             try {
                 const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/2023.5.8' } };
-                const response = await fetch(getApiUrlFinal(`/clientes/l?investor=${queryParameter}`), options);
+                const response = await fetch(getApiUrlFinal(`/clientes/l?email=${queryParameter}`), options);
 
                 const responseData = await response.json();
                 setResponseData(responseData);
