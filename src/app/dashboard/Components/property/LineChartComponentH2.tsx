a7ebf0122c026248ed9b3701eeb28db0c3595d60
aarevaloe <121042571+aarevaloe@users.noreply.github.com>
2024-02-20 15:38:46 -0500

    Ajuste api H2

--- src/app/dashboard/Components/property/LineChartComponentH2.tsx
+++ src/app/dashboard/Components/property/LineChartComponentH2.tsx
@@ -32,22 +32,7 @@ interface Item {
 
 const LineChartComponentH2 = () => {
     const { userEmail } = useAuth();
-    const getQueryParameter = (userEmail: string | null): string => {
-        if (!userEmail) {
-            // En caso de que el correo electrónico no esté disponible
-            return "";
-        }
-        // Verifica el correo electrónico y devuelve el parámetro de consulta correspondiente
-        if (userEmail === "fcortes@duppla.co" || userEmail === "fernando@skandia.co") {
-            return "skandia";
-        } else if (userEmail === "aarevalo@duppla.co" || userEmail === "fernando@weseed.co") {
-            return "weseed";
-        } else if (userEmail === "scastaneda@duppla.co") {
-            return "disponible";
-        } 
-        // En caso de que el correo electrónico no coincida con ninguno de los casos anteriores
-        return "";
-    };
+  
     const [data, setData] = useState<DataType>({ ult_12_meses: [], este_anho: [], ult_6_meses: [] });
     const [selectedDataKeyH2, setSelectedDataKeyH2] = useState<string>('ult_12_meses');
     const [selectedValue, setSelectedValue] = useState<string | number>('ult_12_meses');
@@ -61,12 +46,15 @@ const LineChartComponentH2 = () => {
 
     useEffect(() => {
 
-        const queryParameter = getQueryParameter(userEmail);
+        const queryParameter = userEmail;
 
         const fetchData = async () => {
+            if (!userEmail) {
+                return;
+            }
             try {
                 const options = { method: 'GET', headers: { 'User-Agent': 'insomnia/2023.5.8' } };
-                const response = await fetch(getApiUrlFinal(`/inmuebles/h2?investor=${queryParameter}`), options);
+                const response = await fetch(getApiUrlFinal(`/inmuebles/h2?email=${queryParameter}`), options);
                 
                 const newData = await response.json();
 
